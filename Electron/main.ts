5c06e0a5a15f2ec407e6bb9e6a6de0aea84a14f3
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-10-19 19:24:33 +0800

    Project Setup For Development

--- Electron/main.ts
+++ Electron/main.ts
@@ -1,25 +1,29 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, BrowserWindowConstructorOptions } from "electron";
+import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 import path from "path";
-
 const isDev = process.env.IS_DEV == "true" ? true : false;
 
-function createWindow() {
-    // Create the browser window.
+async function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             preload: __dirname + "/preload.js",
         },
-    });
+        show: false,
+        alwaysOnTop: true,
+    } as BrowserWindowConstructorOptions);
 
     // and load the index.html of the app.
     // win.loadFile("index.html");
-    mainWindow.loadURL(
-        isDev
-            ? "http://localhost:3000"
-            : `file://${path.join(__dirname, "./index.html")}`
-    );
+    await mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "./index.html")}`);
+    mainWindow.show();
+
+    // this will turn off always on top after opening the application
+    setTimeout(() => {
+        mainWindow.setAlwaysOnTop(false);
+    }, 1000);
+
     // Open the DevTools.
     if (isDev) {
         mainWindow.webContents.openDevTools();
@@ -30,6 +34,18 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+    // if dev
+
+    if (isDev) {
+        installExtension(VUEJS_DEVTOOLS)
+            .then(() => {
+                console.log("Added Extension");
+            })
+            .catch((err) => {
+                console.log("Extension Error: ", err);
+            });
+    }
+
     createWindow();
     app.on("activate", function () {
         // On macOS it's common to re-create a window in the app when the
--- FrontEndApp/vite.config.ts
+++ FrontEndApp/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     build: {
         outDir: "./../dist",
         chunkSizeWarningLimit: 1000,
     },
-    base: process.env.ELECTRON == "true" ? "./" : ".",
+    base: mode == "development" ? "" : "./",
     plugins: [vue()],
     server: {
         port: 3000,
     },
-});
+}));
