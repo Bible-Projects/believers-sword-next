780cc34315da3a15f2ebc63f967c8a3448803cc5
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-10-21 17:19:12 +0800

    setup front end

---
+++ FrontEndApp/src/router/router.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import PrayerList from "./../Views/PrayerList/PrayerList.vue";
+
+export const routes: Array<RouteRecordRaw> = [
+    {
+        path: "/",
+        redirect: "prayer-list",
+    },
+    {
+        name: "PrayerList",
+        path: "/prayer-list",
+        component: PrayerList,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
