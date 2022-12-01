68b0ba351c2a39c44a92ab1ac5ad2ce0c158aa39
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-12-01 17:20:09 +0800

    set slot on Right side container

--- FrontEndApp/src/Views/ReadBible/RightSideBar/Bibles/Bibles.vue
+++ FrontEndApp/src/Views/ReadBible/RightSideBar/Bibles/Bibles.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from 'vue';
 import { getDownloadedBible } from '../../../../util/Modules/Controller/FeBibleController';
-import { NCheckbox, NCheckboxGroup, useMessage } from 'naive-ui';
+import { NCheckbox, NCheckboxGroup, useMessage, NIcon, NButton } from 'naive-ui';
 import RightSideBarContainer from './../../../../components/ReadBible/RightSideBarContainer.vue';
 import { useBibleStore } from '../../../../store/BibleStore';
 import { useModuleStore } from '../../../../store/moduleStore';
+import { useBibleDownloadStore } from '../../../../store/downloadBible';
+import { Download } from '@vicons/carbon';
 
 const message = useMessage();
 const bibleStore = useBibleStore();
 const moduleStore = useModuleStore();
+const bibleDownloadStore = useBibleDownloadStore();
 
 function handleCheckBox(bibleVersions: Array<any>) {
     if (!bibleVersions.length) {
@@ -20,6 +23,15 @@ function handleCheckBox(bibleVersions: Array<any>) {
 </script>
 <template>
     <RightSideBarContainer title="Bible List">
+        <template #rightSide>
+            <NButton quaternary size="tiny" title="Download" @click="bibleDownloadStore.showBibleDownloadModal = true">
+                <template #icon>
+                    <NIcon>
+                        <Download />
+                    </NIcon>
+                </template>
+            </NButton>
+        </template>
         <NCheckboxGroup v-model:value="bibleStore.selectedBibleVersions" @update:value="handleCheckBox">
             <template v-for="bible in moduleStore.bibleLists">
                 <NCheckbox :value="bible.file_name" :label="bible.title" />
--- FrontEndApp/src/components/ReadBible/RightSideBarContainer.vue
+++ FrontEndApp/src/components/ReadBible/RightSideBarContainer.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import { NIcon, NButton } from 'naive-ui';
-import { Download } from '@vicons/carbon';
-import { useBibleDownloadStore } from '../../store/downloadBible';
-const bibleDownloadStore = useBibleDownloadStore();
 defineProps({
     title: {
         type: String,
@@ -13,15 +9,9 @@ defineProps({
     <div class="w-full h-full">
         <div class="h-30px select-none flex justify-between">
             <h1 class="font-700">{{ $props.title }}</h1>
-            <div>
-                <NButton quaternary size="tiny" title="Download" @click="bibleDownloadStore.showBibleDownloadModal = true">
-                    <template #icon>
-                        <NIcon>
-                            <Download />
-                        </NIcon>
-                    </template>
-                </NButton>
-            </div>
+            <slot name="rightSide">
+                <div></div>
+            </slot>
         </div>
         <div class="w-full h-[calc(100%-30px)] overflow-auto overflowing-div">
             <slot />
