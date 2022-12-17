45e75b174112b2d4568c0660d5588522f2cdf58d
Pe Paulo Giovanni <paulusiohannes@gmail.com>
2022-12-17 14:45:48 -0300

    Portuguese translation
    Add Portuguese translation

---
+++ FrontEndApp/src/util/Internationalization/lang/pt.ts
@@ -0,0 +1,24 @@
+import { InternationalMessageType } from './InternationalMessage';
+
+const message: InternationalMessageType = {
+    title: `Espada dos Fiéis`,
+    'read-bible': 'Ler a Bíbia',
+    Sermons: 'Homilias',
+    'Prayer List': 'Lista de Oração',
+    About: 'Sobre',
+    Profile: 'Perfil',
+    Settings: 'Configurações',
+    'Search Bible': 'Pesquisar Bíblia',
+    'Bible List': 'Lista de Bíblias',
+    Bookmarks: 'Marcadores',
+    Search: 'Pesquisar',
+    'Are You sure?': 'Você está certo?',
+    Before: 'Anterior',
+    Next: 'Próximo',
+    'Bible Versions': 'Versões da Bíblia',
+    Highlights: 'Destaques',
+    Nightly: 'Noturno',
+    copy: 'copiar',
+    remove: 'remover',
+};
+export default message;
