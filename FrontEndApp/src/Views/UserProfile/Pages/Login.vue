776eddeb09e11c73cd6409e620ea190b12154b3d
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-04-19 06:25:25 +0800

    Added Profile Page

--- FrontEndApp/src/Views/UserProfile/Pages/Login.vue
+++ FrontEndApp/src/Views/UserProfile/Pages/Login.vue
@@ -1,15 +1,113 @@
-<script setup lang="ts">
-import { NInput, NButton } from 'naive-ui';
+<script lang='ts' setup>
+import { NButton, NInput, useMessage } from 'naive-ui';
+import { onMounted, reactive, ref } from 'vue';
+import { supabase } from '../../../util/SupaBase/SupaBase';
+import { useRouter } from 'vue-router';
+import { useUserStore } from '../../../store/userStore';
+import { isSignedIn } from '../../../util/SupaBase/Auth/Auth';
+
+const isRegister = ref(false);
+const loading = ref(false);
+const userStore = useUserStore();
+const router = useRouter();
+const message = useMessage();
+const form = reactive<{
+    email: string | null,
+    password: string | null,
+    retypePassword: string | null
+}>({
+    email: null,
+    password: null,
+    retypePassword: null
+});
+
+async function login() {
+    if (!form.email || !form.password) {
+        message.error('Cant Continue, some fields does not exist.');
+        return;
+    }
+
+    loading.value = true;
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: form.email,
+        password: form.password
+    });
+    loading.value = false;
+
+    if (error) {
+        message.error(error.message);
+        return;
+    }
+
+    userStore.user = data;
+    message.success('Logged In! Successfully.');
+    await router.push('/profile/profile');
+}
+
+async function register() {
+    if (!form.email || !form.password) {
+        message.error('Cant Continue, some fields does not exist.');
+        return;
+    }
+
+    if (form.password != form.retypePassword) {
+        message.error('Password and retype password should be the same.');
+        return;
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+        email: form.email,
+        password: form.password
+    });
+
+    if (error) {
+        message.error(error.message);
+        return;
+    }
+
+
+}
+
+async function submit() {
+    if (isRegister.value) {
+        await register();
+    } else {
+        await login();
+    }
+}
+
+onMounted(async () => {
+    loading.value = true;
+    if (userStore.user) {
+        await router.push('/profile/profile');
+    } else {
+        const data = await isSignedIn();
+        if (data) {
+            userStore.user = data;
+            await router.push('/profile/profile');
+        }
+    }
+    loading.value = false;
+});
 </script>
 <template>
-    <div class="h-full w-full">
-        <div class="w-400px mx-auto mt-5 flex flex-col gap-2">
-            <h5 class="text-center font-800 text-size-25px">User Login</h5>
+    <div class='h-full w-full'>
+        <div class='w-400px mx-auto mt-5 flex flex-col gap-2'>
+            <h5 class='text-center font-800 text-size-25px'>
+                {{ isRegister ? 'Sign Up' : 'Sign In' }}
+            </h5>
             Email Address:
-            <NInput placeholder="Email" />
+            <NInput v-model:value='form.email' placeholder='Email' />
             Password:
-            <NInput placeholder="Password" type="password" />
-            <NButton>Login</NButton>
+            <NInput v-model:value='form.password' placeholder='Password' type='password' />
+            Retype Password:
+            <NInput v-if='isRegister' v-model:value='form.retypePassword' placeholder='Retype Password' type='password' />
+            <NButton :disabled='loading' :loading='loading' type='primary' @click='login'
+                     @keydown.enter='submit'>
+                {{ isRegister ? 'Sign Me Up' : 'Sign In' }}
+            </NButton>
+            <NButton v-show='!isRegister' @click='isRegister = true'>Create Account</NButton>
+            <NButton v-show='isRegister' @click='isRegister = false'>Already Have an Account?</NButton>
         </div>
     </div>
 </template>
--- FrontEndApp/src/Views/UserProfile/Pages/Profile/Profile.vue
+++ FrontEndApp/src/Views/UserProfile/Pages/Profile/Profile.vue
@@ -1,3 +1,60 @@
+<script lang='ts' setup>
+import { onMounted, ref } from 'vue';
+import { isSignedIn } from '../../../../util/SupaBase/Auth/Auth';
+import { useUserStore } from '../../../../store/userStore';
+import { useRouter } from 'vue-router';
+import { supabase } from '../../../../util/SupaBase/SupaBase';
+import { NButton, useDialog, useMessage } from 'naive-ui';
+
+const loading = ref(false);
+const userStore = useUserStore();
+const router = useRouter();
+const message = useMessage();
+const dialog = useDialog();
+
+onMounted(async () => {
+    if (!userStore.user) {
+        const data = await isSignedIn();
+        if (data) {
+            userStore.user = data;
+            await router.push('/profile/profile');
+        } else {
+            await router.push('/profile');
+        }
+    }
+});
+
+async function logout() {
+    dialog.warning({
+        title: 'Confirm',
+        content: 'Are you sure to logout?',
+        positiveText: 'Yes',
+        negativeText: 'No',
+        onPositiveClick: async () => {
+            const { error } = await supabase.auth.signOut();
+
+            if (error) {
+                message.error(error.message);
+                return;
+            }
+
+            userStore.user = null;
+            await router.push('/profile');
+        }
+    });
+}
+</script>
+
 <template>
-    This is the profile template
+    <div class='flex justify-center items-center h-[80vh]'>
+        <div class='flex flex-col items-center gap-3'>
+            <div>You are Logged.</div>
+            <div>{{ userStore.user ? userStore.user.user.email : '--' }}</div>
+            <div>
+                <NButton :disabled='loading' :loading='loading' secondary strong type='warning' @click='logout'>
+                    Logout
+                </NButton>
+            </div>
+        </div>
+    </div>
 </template>
