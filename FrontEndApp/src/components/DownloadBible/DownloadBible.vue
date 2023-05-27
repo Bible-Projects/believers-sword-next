cbccb4005e2b22abcf60261e787852209c4fe1d3
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-05-27 14:48:11 +0800

    update the download functionality force to download per version only

--- FrontEndApp/src/components/DownloadBible/DownloadBible.vue
+++ FrontEndApp/src/components/DownloadBible/DownloadBible.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { NModal, NCard, NButton, NAlert, NCheckbox, NCheckboxGroup } from 'naive-ui';
+import { NModal, NCard, NButton, NIcon } from 'naive-ui';
 import { computed, ref } from 'vue';
 import { useBibleDownloadStore } from '../../store/downloadBible';
 import { useModuleStore } from '../../store/moduleStore';
-import { bible } from './../../util/modules';
+import { bible } from '../../util/modules';
+import { Download } from '@vicons/carbon';
 
 const bibleDownloadStore = useBibleDownloadStore();
 const moduleStore = useModuleStore();
-const urlsToDownload = ref<null | Array<string>>(null);
 const BibleVersions = computed(() => {
     return bible.sort((a, b) => (a.title as any) - (b.title as any));
 });
@@ -19,15 +19,17 @@ function isAlreadyDownloaded(file_name: string) {
     return true;
 }
 
-function clickDownload() {
-    bibleDownloadStore.isDownloading = true;
+const downloadLoading = ref(false);
+const selectedDownloadLink = ref<string | null>(null);
+function clickDownload(downloadLink: string) {
+    selectedDownloadLink.value = downloadLink;
 
+    bibleDownloadStore.isDownloading = true;
+    downloadLoading.value = true;
     window.browserWindow.downloadModule({
-        urls: urlsToDownload.value?.map((url) => url),
-        progress: (data: any) => {
-            // percentage.value = data.percent * 100;
-        },
+        urls: [downloadLink],
         done: async () => {
+            downloadLoading.value = false;
             await moduleStore.getBibleLists();
             bibleDownloadStore.isDownloading = false;
         },
@@ -37,35 +39,38 @@ function clickDownload() {
 <template>
     <NModal v-model:show="bibleDownloadStore.showBibleDownloadModal">
         <NCard style="width: 600px" :bordered="false" size="small" role="dialog" aria-modal="true">
-            <div class="flex justify-between items-center mb-2">
+            <div class="flex justify-between items-center">
                 <h1 class="font-800 select-none">Available Versions</h1>
-                <NButton
-                    size="small"
-                    :disabled="bibleDownloadStore.isDownloading"
-                    :loading="bibleDownloadStore.isDownloading"
-                    @click="clickDownload()"
-                >
-                    Download Selected
-                </NButton>
             </div>
-            <NAlert type="warning"> Can Download One Item at a Time. </NAlert>
             <div v-if="BibleVersions.length" class="select-none h-[70vh] overflow-y-auto overflowing-div">
-                <NCheckboxGroup
+                <div
+                    v-for="version in BibleVersions"
                     :disabled="bibleDownloadStore.isDownloading"
-                    v-model:value="urlsToDownload"
-                    class="flex flex-col gap-10px py-5"
+                    class="flex gap-10px items-center my-3"
                 >
-                    <template v-for="version in BibleVersions">
-                        <NCheckbox :disabled="!isAlreadyDownloaded(version.file_name)" :value="version.download_link">
-                            <div>
-                                "{{ version.title }}" -
-                                <span class="italic">
-                                    lang: <span class="font-700">{{ version.language_full }}</span>
-                                </span>
-                            </div>
-                        </NCheckbox>
-                    </template>
-                </NCheckboxGroup>
+                    <div :value="version.download_link">
+                        <div>
+                            "{{ version.title }}" -
+                            <span class="italic">
+                                lang: <span class="font-700">{{ version.language_full }}</span>
+                            </span>
+                        </div>
+                    </div>
+                    <NButton
+                        size="tiny"
+                        :disabled="!isAlreadyDownloaded(version.file_name) || downloadLoading"
+                        :type="!isAlreadyDownloaded(version.file_name) ? 'default' : 'info'"
+                        @click="clickDownload(version.download_link)"
+                        secondary
+                        rounded
+                        :loading="version.download_link === selectedDownloadLink && downloadLoading"
+                    >
+                        <NIcon>
+                            <Download />
+                        </NIcon>
+                        Download
+                    </NButton>
+                </div>
             </div>
             <div v-else class="flex items-center justify-center h-[80vh]">It seems you have downloaded all the modules 😊</div>
         </NCard>
