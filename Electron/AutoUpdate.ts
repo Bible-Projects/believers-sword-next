3dabeafbe35b80e970a68cce4a2afea427d1e05f
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-08-07 18:27:12 +0800

    Update AutoUpdate.ts

--- Electron/AutoUpdate.ts
+++ Electron/AutoUpdate.ts
@@ -1,18 +1,34 @@
-import { dialog, app } from 'electron';
+import { app, dialog } from 'electron';
 import { autoUpdater, UpdateInfo } from 'electron-updater';
 
 export default async () => {
     if (app.isPackaged) {
+        autoUpdater.autoDownload = false;
         autoUpdater.autoInstallOnAppQuit = true;
-        await autoUpdater.checkForUpdatesAndNotify();
+        await autoUpdater.checkForUpdates();
+
+        autoUpdater.on('update-available', () => {
+            // Show a dialog to the user indicating that an update is available
+            dialog.showMessageBox({
+                type: 'info',
+                buttons: ['Download Update', 'Cancel'],
+                title: 'Update Available',
+                message: 'A new update is available. Do you want to download and install it?'
+            }).then((response) => {
+                if (response.response === 0) {
+                    // If the user clicks 'Download Update'
+                    autoUpdater.downloadUpdate();
+                }
+            });
+        });
+
         autoUpdater.addListener('update-downloaded', (info: UpdateInfo) => {
             dialog
                 .showMessageBox({
-                    title: 'Restart Believers Sword?',
-                    type: 'question',
-                    message: `New version "Believers Sword ${info.version}" has been successfully downloaded.`,
-                    buttons: ['Yes', 'Later', 'Yes, Update'],
-                    cancelId: 1,
+                    type: 'info',
+                    buttons: ['Install and Restart', 'Later'],
+                    title: 'Update Ready',
+                    message: 'The update has been downloaded. Do you want to install and restart the application?'
                 })
                 .then(({ response }) => {
                     if (response == 0 || response == 2) {
@@ -21,4 +37,20 @@ export default async () => {
                 });
         });
     }
+
+    autoUpdater.on('error', (error) => {
+        // Handle update error
+        dialog
+            .showMessageBox({
+                type: 'error',
+                buttons: ['OK'],
+                title: 'Error Updating!',
+                message: 'Their is an Error Updating, the developer is working on a fix right now.'
+            })
+            .then(({ response }) => {
+                if (response == 0 || response == 2) {
+                    autoUpdater.quitAndInstall();
+                }
+            });
+    });
 };
