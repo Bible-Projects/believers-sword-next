98e00aad7dc6d89a7e174e92e51c97ed80103495
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-08-28 10:19:00 +0800

    Update

--- FrontEndApp/src/store/userStore.ts
+++ FrontEndApp/src/store/userStore.ts
@@ -1,12 +1,20 @@
 import { defineStore } from 'pinia';
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 export const useUserStore = defineStore('useUserStore', () => {
     const user = ref<any>(null);
     const user_id = computed(() => {
         return user.value?.user?.id ?? null;
     });
-    const syncData = ref<boolean>(false)
+    const syncData = ref<boolean>(false);
+
+    function syncData () {
+        window.syncDataOnline = setTimeout()
+    }
+
+    watch(() => syncData.value, (isSyncData) => {
+
+    });
 
     return {
         user,
--- FrontEndApp/src/util/Sync/sync.ts
+++ FrontEndApp/src/util/Sync/sync.ts
@@ -1,5 +1,20 @@
 export async function syncData() {
-    const bookmarks =  await window.browserWindow.getBookMarks();
-    const highlights = await window.browserWindow.getHighlights();
-    const clipNotes = await window.browserWindow.getClipNotes();
+    // get all data
+    const bookmarks = await window.browserWindow.getBookMarks();
+    const highlights = await window.browserWindow.getHighlights(JSON.stringify({
+        page: 1,
+        search: null,
+        limit: 9999999999
+    }));
+    const clipNotes = await window.browserWindow.getClipNotes(JSON.stringify({
+        page: 1,
+        search: null,
+        limit: 9999999999
+    }));
+
+
+    // set data
+    console.log('bookmarks', bookmarks);
+    console.log('highlights', highlights);
+    console.log('clipNotes', clipNotes);
 }
