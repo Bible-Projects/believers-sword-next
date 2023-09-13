934cd7161080c6350e629537a2378a2b53b81f20
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-09-13 21:11:41 +0800

    Adjusted and removed some modules

---
+++ FrontEndApp/src/util/Modules/Commentaries/new-international-version.commentaries.ts
@@ -0,0 +1,10 @@
+export default {
+    file_name: `New International Version.commentaries.SQLite3`,
+    language: `en`,
+    language_full: `english`,
+    title: `New International Version Commentaries`,
+    description: `New International Version 2011 – <br/>
+    The Holy Bible, New International Version® NIV® Copyright © 1973, 1978, 1984, 2011 by Biblica, Inc.® Used by Permission of Biblica, Inc.® All rights reserved worldwide. <br/>
+    English`,
+    download_link: `https://github.com/Bible-Projects/believers-sword-next/raw/main/Modules/Commentaries/New International Version.commentaries.SQLite3`,
+};
--- FrontEndApp/src/util/modules.ts
+++ FrontEndApp/src/util/modules.ts
@@ -1,21 +1,17 @@
-import AB1978Commentaries from './Modules/Commentaries/AB1978.commentaries';
-import ASNDCommentaries from './Modules/Commentaries/ASND.commentaries';
-import BamdCommentaries from './Modules/Commentaries/BAMD.commentaries';
-import ESVCommentaries from './Modules/Commentaries/ESV.commentaries';
 import americanStandardVersion1901 from './Modules/Bible/american-standard-version-1901';
 import angBiblia1978 from './Modules/Bible/ang-biblia-1978';
 import angSalitaNgDios2015 from './Modules/Bible/ang-salita-ng-dios-2015';
 import bibliaAveMaria1959 from './Modules/Bible/biblia-ave-maria-1959';
-import bibliaDeJoaoFerreiraDeAlmeidaEdicaoDe1819
-    from './Modules/Bible/biblia-de-joao-ferreira-de-almeida-edicao-de-1819';
+import bibliaDeJoaoFerreiraDeAlmeidaEdicaoDe1819 from './Modules/Bible/biblia-de-joao-ferreira-de-almeida-edicao-de-1819';
 import catholicPublicDomainVersion2009 from './Modules/Bible/catholic-public-domain-version-2009';
 import englishStandardVersion20012016 from './Modules/Bible/english-standard-version-2001-2016';
-import kingJamesBible1611PureCambridgeEdition1900
-    from './Modules/Bible/king-james-bible-1611-pure-cambridge-edition-1900';
+import kingJamesBible1611PureCambridgeEdition1900 from './Modules/Bible/king-james-bible-1611-pure-cambridge-edition-1900';
 import kingJamesVersion1769 from './Modules/Bible/king-james-version-1769';
 import kingJamesVersionOf1611Or1769 from './Modules/Bible/king-james-version-of-1611-or-1769';
 import magandangBalitaBiblia from './Modules/Bible/magandang-balita-biblia';
 import newAmericanStandardBible from './Modules/Bible/new-american-standard-bible';
+import newInternationalVersion from './Modules/Bible/new-international-version';
+import newInternationalVersionCommentaries from './Modules/Commentaries/new-international-version.commentaries';
 
 export const bible = [
     americanStandardVersion1901,
@@ -29,6 +25,7 @@ export const bible = [
     kingJamesVersion1769,
     kingJamesVersionOf1611Or1769,
     magandangBalitaBiblia,
-    newAmericanStandardBible
+    newAmericanStandardBible,
+    newInternationalVersion,
 ];
-export const commentaries = [AB1978Commentaries, ASNDCommentaries, BamdCommentaries, ESVCommentaries];
+export const commentaries = [newInternationalVersionCommentaries];
