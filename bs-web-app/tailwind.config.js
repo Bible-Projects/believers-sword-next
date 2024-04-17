85c13efef9001918c6beb12395905c879f858bc8
Jenuel Ganawed <jenuelganawed936@gmail.com>
2024-04-17 23:18:47 +0800

    Update added poppins

--- bs-web-app/tailwind.config.js
+++ bs-web-app/tailwind.config.js
@@ -89,6 +89,7 @@ module.exports = {
             },
             fontFamily: {
                 mulish: ["Mulish Regular"],
+                poppins: ["Poppins"],
             },
         },
     },
