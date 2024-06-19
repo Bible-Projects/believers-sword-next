27a098e1ed771adbdc78d7f6bbf422678515e727
Jenuel Ganawed <jenuelganawed936@gmail.com>
2024-06-19 18:29:55 +0800

    setup web-next

--- web-next/astro.config.mjs
+++ web-next/astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
+    integrations: [
+        UnoCSS({
+            injectReset: true,
+        }),
+    ],
 });
