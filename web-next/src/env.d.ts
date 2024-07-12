6fd6b5b81bf1db1cb630d421990eb2841ab5abcf
Jenuel Oras Ganawed <me@jenuel.dev>
2024-07-12 23:06:15 +0800

    Added New Content

--- web-next/src/env.d.ts
+++ web-next/src/env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly SUPABASE_URL: string;
+    readonly SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
