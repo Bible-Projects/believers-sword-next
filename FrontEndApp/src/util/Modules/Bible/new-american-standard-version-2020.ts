f7596be96f5b407a5b6012abae2a87c4c350088c
Jenuel Oras Ganawed <me@jenuel.dev>
2024-07-13 23:37:18 +0800

    Updates

---
+++ FrontEndApp/src/util/Modules/Bible/new-american-standard-version-2020.ts
@@ -0,0 +1,8 @@
+export default {
+    file_name: `New American Standard Version 2020.SQLite3`,
+    language: `en`,
+    language_full: `english`,
+    title: `New American Standard Version 2020`,
+    description: `The New American Standard Bible (NASB) 2020 is an updated version of the New American Standard Bible, a widely respected English translation of the Bible. The NASB 2020 aims to improve readability and comprehension while maintaining a high degree of literal accuracy to the original Hebrew, Aramaic, and Greek texts.`,
+    download_link: `https://github.com/Bible-Projects/believers-sword-next/raw/main/Modules/Bible/New American Standard Version 2020.SQLite3`,
+};
---
+++ FrontEndApp/src/util/Modules/Commentaries/new-american-standard-version-2020-commentaries.ts
@@ -0,0 +1,8 @@
+export default {
+    file_name: `New American Standard Version 2020 Commentaries.SQLite3`,
+    language: `en`,
+    language_full: `english`,
+    title: `New American Standard Version 2020 Commentaries`,
+    description: `Commentaries on the New American Standard Bible (NASB) 2020 are scholarly works that provide explanations, interpretations, and context for the text of the Bible. These commentaries help readers understand the historical background, cultural setting, and theological significance of the scriptures.`,
+    download_link: `https://github.com/Bible-Projects/believers-sword-next/raw/main/Modules/Commentaries/New American Standard Version 2020 Commentaries.SQLite3`,
+};
