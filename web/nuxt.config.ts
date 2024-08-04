cf89a32a386c847527eee3b7af95338fd29bae33
Jenuel Oras Ganawed <me@jenuel.dev>
2024-08-04 19:43:07 +0800

    set the nuxt to nuxt 4 compatibility

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+    ssr: true,
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    future: {
+        compatibilityVersion: 4,
+    },
+});
